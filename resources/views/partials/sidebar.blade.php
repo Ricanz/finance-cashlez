db8c1196fab9cd7bbba3c257a6f2bcee345b6a14
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2024-05-17 11:25:42 +0700

    fix: sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -79,7 +79,7 @@
                     </div> --}}
                     <div class="menu-item">
                         <div class="menu-content pt-8 pb-2">
-                            <span class="menu-section text-muted text-uppercase fs-8 ls-1">Master Data Farhan</span>
+                            <span class="menu-section text-muted text-uppercase fs-8 ls-1">Master Data</span>
                         </div>
                     </div>
                     <div class="menu-item">
